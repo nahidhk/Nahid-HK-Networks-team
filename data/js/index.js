f76941576381dd160f3ca23c61fc2cb59c3d5de2
nahidhk <nahidappltd@gmail.com>
2024-02-23 10:44:57 +0600

    new update

--- data/js/index.js
+++ data/js/index.js
@@ -1,3 +1,15 @@
 function myappio(){
   document.getElementById("loadio").style.display='none';
 }
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+});
